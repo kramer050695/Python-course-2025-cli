48ac6550e9375f99a9d78f9f46358388baf2d8f0
Zaguzov <kramer050695@gmail.com>
2025-02-18 15:39:17 +0300

    Add example of naive arg parcing

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import sys
+import argparse
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+commands = {"add": add, "sub": sub}
+
+command = sys.argv[1]
+x = int(sys.argv[2])
+y = int(sys.argv[3])
+
+commands[command](x, y)
+print(sys.argv)
+print(commands[command](x, y))
